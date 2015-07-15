7c4f486c39ed615ac043258b36a74bbeb0925564
Feng Jau <crow1984@yeah.net>
2015-07-16 00:28:02 +0800

    init commit

--- src/test/scala-2.11/ManageVersionSpec.scala
+++ src/test/scala-2.11/ManageVersionSpec.scala
@@ -5,8 +5,9 @@ class ManageVersionSpec extends Specification {
   "Manage version" should {
 
     "a test for latest" in {
-      ManageVersion.latest(List("0.13.7", "0.9.10", "0.9.11-RC1")) must_== ("0.13.7")
+      ManageVersion.latest(List("0.13.7", "0.9.10", "0.9.11-RC1")) mustEqual ("0.13.7")
 
+      ManageVersion.latest(List("0.13.6", "0.13.7-RC1", "0.13.7-RC2")) mustEqual ("0.13.7-RC2")
     }
   }
 
